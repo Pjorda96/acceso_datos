1b777a26d83b13693d34ffd13371da07d7bddb72
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2019-10-29 23:49:07 +0100

    new project pending to rename the folder

---
+++ proyectoplaceMyBet/placeMyBet/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace placeMyBet.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ proyectoplaceMyBet/placeMyBet/Controllers/PartidoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PlaceMyBet.Models;
+
+namespace PlaceMyBet.Controllers
+{
+    public class PartidoController : ApiController
+    {
+        // GET: api/Partido
+        public IEnumerable<PartidoDTO> Get()
+        {
+            var repo = new PartidoRepository();
+            List<PartidoDTO> partidos = repo.RetrieveDTO();
+
+            return partidos;
+        }
+
+        // GET: api/Partido/5
+        public Partido Get(int id)
+        {
+            //var repo = new PartidoRepository();
+            //Partido p = repo.Retrieve();
+            return null;
+        }
+
+        // POST: api/Partido
+        public void Post([FromBody]Partido partido)
+        {
+            var repo = new PartidoRepository();
+            repo.Save(partido);
+        }
+
+        // PUT: api/Partido/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Partido/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
