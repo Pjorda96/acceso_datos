ab945d80657b3e5cb68182eb3c29382b729d9c40
pabloPc <pjorda96@gmail.com>
2020-01-19 16:32:38 +0100

    ae10

--- QuickTrade/src/app/services/element.service.ts
+++ QuickTrade/src/app/services/element.service.ts
@@ -1,34 +1,32 @@
 import {Injectable} from '@angular/core';
-import { data } from '../../global/storage';
 import {IData, IHogar, IInmobiliaria, IMotor, ITecnologia} from '../interfaces';
-import {AngularFireDatabaseModule} from '@angular/fire/database';
-/*import * as firebase from 'firebase/src';
-import { environment } from '../../environments/environment';*/
+import {AngularFireDatabase} from '@angular/fire/database';
 
 @Injectable()
 export class ElementService {
   constructor(
-    private db: AngularFireDatabaseModule,
+    private db: AngularFireDatabase,
   ) { }
 
-  getElements(): (IData | IMotor | IInmobiliaria | ITecnologia | IHogar)[] {
-    const elements = this.db.database.ref('elements');
-    const elem: (IData | IMotor | IInmobiliaria | ITecnologia | IHogar)[] = [];
-    elements.once('value', snapshot => snapshot.forEach(child => elem.push(child.val())));
-    return elem;
+  getElements(): firebase.database.Reference {
+    return this.db.database.ref('elements');
   }
 
-  // getElement(id: number): (IMotor | IInmobiliaria | ITecnologia | IHogar) {
-  //   console.log(data.filter(el => el.id === id));
-  //   return data.filter(el => el.id === id)[0];
-  // }
+  /*getElement(id: number): (IMotor | IInmobiliaria | ITecnologia | IHogar) {
+    console.log(data.filter(el => el.id === id));
+    return data.filter(el => el.id === id)[0];
+  }*/
 
-  // putElement(element: (IMotor | IInmobiliaria | ITecnologia | IHogar | IData)): void {
-  //   data.push(element);
-  // }
+  getElement(key): firebase.database.Reference {
+    return this.db.database.ref(key);
+  }
 
-  setElement(element: IData | IMotor | IInmobiliaria | ITecnologia | IHogar): void {
+  /*putElement(element: (IMotor | IInmobiliaria | ITecnologia | IHogar | IData)): void {
+    data.push(element);
+  }*/
+
+  setElement(element: IData | IMotor | IInmobiliaria | ITecnologia | IHogar): firebase.database.Reference {
     const dbRef = this.db.database.ref('elements');
-    dbRef.push(element);
+    return dbRef.push(element);
   }
 }
